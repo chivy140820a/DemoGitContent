a9ff47f46ac2d2f28823de8492ab520ed55bb3bf
chivyh1408@gmail.com <chivy1408@gmail.com>
2022-04-18 18:56:06 +0700

    init

---
+++ DemoContentGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoContentGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
