0f590e0a66783fc64e527c2a5936d8745618ba39
chivyh1408@gmail.com <chivy1408@gmail.com>
2022-04-18 19:55:30 +0700

    updateprice

--- DemoContentGit/bdadada/Content.cs
+++ DemoContentGit/bdadada/Content.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal LastPrice { get; set; }
     }
 }
